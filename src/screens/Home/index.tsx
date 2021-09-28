d822c0748b98ac399e7859781c3b561c7ed8d922
Odair <odairjgn@outlook.com>
2021-09-27 23:31:19 -0300

    Ajuste

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -12,6 +13,27 @@ import {
 } from './styles';
 
 export function Home() {
+
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'http://4.bp.blogspot.com/-IkezPUE2c0I/UqHHDMG6XdI/AAAAAAAAF5Y/abl6wQb1atU/s1600/carro1.png'
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'http://1.bp.blogspot.com/-xUWtd8c4au4/UqHHJ6LNo5I/AAAAAAAAF6E/QXrWCEb8CpU/s1600/png_carro1.png'
+  };
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +52,11 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
+
+
     </Container >
   );
 }

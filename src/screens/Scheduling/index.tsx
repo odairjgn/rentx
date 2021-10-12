3d6f323c7980f60eb94ea50bf60b16abf6f3005e
Odair <odairjgn@outlook.com>
2021-10-11 23:24:22 -0300

    Ajustes

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -17,6 +17,7 @@ import {
 } from './styles';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 export function Scheduling() {
 
@@ -62,7 +63,7 @@ export function Scheduling() {
             </Header>
 
             <Content>
-
+                <Calendar />
             </Content>
 
             <Footer>

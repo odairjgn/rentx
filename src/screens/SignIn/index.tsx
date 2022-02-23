aa479a728ac9c31124d30c97e61ef2e46fcf7d8c
Odair <odairjgn@outlook.com>
2022-02-23 00:10:19 -0300

    ajustes

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -39,6 +39,8 @@ export function SignIn() {
             });
 
             await schema.validate({ email, password })
+
+            //Fazer login
         } catch (error) {
             if (error instanceof Yup.ValidationError) {
                 Alert.alert('Opa', error.message);

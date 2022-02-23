8bc8f6855455009ed39ede2fa35af26fd5d5bc08
Odair <odairjgn@outlook.com>
2022-02-23 00:09:12 -0300

    Ajustes

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,8 +4,10 @@ import {
     StatusBar,
     KeyboardAvoidingView,
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native';
+import * as Yup from 'yup';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
@@ -23,6 +25,28 @@ import {
 export function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    async function handleSingIn() {
+        try {
+            const schema = Yup.object().shape({
+                email: Yup
+                    .string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup
+                    .string()
+                    .required('Senha obrigatória')
+            });
+
+            await schema.validate({ email, password })
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                Alert.alert('Opa', error.message);
+            } else {
+                Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, verifique as credenciais.');
+            }
+        }
+    }
 
     return (
         <KeyboardAvoidingView
@@ -66,8 +90,8 @@ export function SignIn() {
                     <Footer>
                         <Button
                             title="Login"
-                            onPress={() => { }}
-                            enabled={false}
+                            onPress={handleSingIn}
+                            enabled={true}
                             loading={false}
                         />
                         <Button

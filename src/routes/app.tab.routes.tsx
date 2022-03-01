5aa2b22f7bf95ea3944d01eabb9990bb3cc27083
Odair José Gambert Nossa <odairjgn@outlook.com>
2022-03-01 18:22:13 -0300

    Ajustes

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -4,10 +4,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeSvg from '../assets/home.svg';
 import CarSvg from '../assets/car.svg';
 import PeopleSvg from '../assets/people.svg';
-
-import { Home } from '../screens/Home';
 import { AppStackRoutes } from './app.stack.routes';
 import { MyCars } from '../screens/MyCars';
+import { Profile } from '../screens/Profile';
 import { Platform } from 'react-native';
 import { useTheme } from 'styled-components';
 
@@ -49,9 +48,9 @@ export function AppTabRoutes() {
                       )
                 }}
             />     
-             <Screen
+            <Screen
                 name="Profile"
-                component={Home}
+                component={Profile}
                 options={{
                     tabBarIcon: ({ focused, color }) => (
                         <PeopleSvg width={24} height={24} fill={color} />
